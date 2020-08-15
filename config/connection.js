69283dd14272d8d62a91bf8868a0d0751e336b5a
srmchartroom <sean@chartroomcreative.com>
2020-08-15 04:38:12 -0400

    Heroku remote db process.env var connection changes

--- config/connection.js
+++ config/connection.js
@@ -7,13 +7,17 @@ This modularizes our connection and prevents unnecessarily bloating and complica
 const mysql = require("mysql");
 const dotenv = require("dotenv").config();
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: "food_db",
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: "food_db",
+  });
+}
 
 // Make connection.
 connection.connect(function (err) {
